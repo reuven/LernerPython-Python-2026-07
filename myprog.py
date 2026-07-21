06a2ba1aeb095f53ee9d5727db33973a3f110927
Reuven Lerner <reuven@lerner.co.il>
2026-07-21 22:06:25 +0300

    gitautosync update 102

--- myprog.py
+++ myprog.py
@@ -6,7 +6,7 @@ print(hello(5))
 print(hello([10, 20, 30]))
 print(hello(hello))
 
-def add(first : int, second : int) -> int:
+def add(first : int|str, second : int|str) -> int|str:
     return first + second
 
 print(add(10, 3))
