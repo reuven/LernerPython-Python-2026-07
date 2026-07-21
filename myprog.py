f46613dbc026f161a787ca8e435c2d663825237d
Reuven Lerner <reuven@lerner.co.il>
2026-07-21 22:05:31 +0300

    gitautosync update 100

--- myprog.py
+++ myprog.py
@@ -5,3 +5,9 @@ print(hello('Reuven'))
 print(hello(5))
 print(hello([10, 20, 30]))
 print(hello(hello))
+
+def add(first, second):
+    return first + second
+
+print(add(10, 3))
+print(add('a', 'b'))
